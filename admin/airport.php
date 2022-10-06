10e6d64f424599fc445a0e3c9b42b4522bc28952
piyumi27 <piyurajakaruna@gmail.com>
2022-10-07 00:36:30 +0530

    create airport

---
+++ admin/airport.php
@@ -0,0 +1,37 @@
+<?php include('db_connect.php');?>
+
+<div class="container-fluid">
+	
+	<div class="col-lg-12">
+		<div class="row">
+			<!-- FORM Panel -->
+			<div class="col-md-4">
+			<form action="" id="manage-airports">
+				<div class="card">
+					<div class="card-header">
+						   Airport's Form
+				  	</div>
+					
+					<div class="card-body">
+							<input type="hidden" name="id">
+							<div class="form-group">
+								<label class="control-label">Airport</label>
+								<textarea name="airport" id="" cols="30" rows="2" class="form-control"></textarea>
+							</div>
+
+							<div class="form-group">
+								<label class="control-label">Location</label>
+								<textarea name="location" id="" cols="30" rows="2" class="form-control"></textarea>
+							</div>
+	
+							
+							
+					</div>
+                </div>	
+            </form>
+            </div>
+        </div>
+    </div>
+</div>
+							
+					

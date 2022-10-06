4a210e1a863c52d75165be78a6f721c4177fb6de
yisirasankalapani4 <sankalpani.jayasuriya@gmail.com>
2022-10-07 00:23:35 +0530

    create login.php

---
+++ Admin/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Admin | Online Flight Booking System</title>
+ 	
+
+<?php include('./header.php'); ?>
+<?php include('./db_connect.php'); ?>
+<?php 
+session_start();
+if(isset($_SESSION['login_id']))
+header("location:index.php?page=home");
+
+?>
+
+</head>
+</html>

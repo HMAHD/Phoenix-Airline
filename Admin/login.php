5b36825d880bbeb1e324f6f127e488e9e64dfa2e
yisirasankalapani4 <sankalpani.jayasuriya@gmail.com>
2022-10-07 00:46:36 +0530

    update login.php

--- Admin/login.php
+++ Admin/login.php
@@ -18,4 +18,60 @@ header("location:index.php?page=home");
 ?>
 
 </head>
+<style>
+	body{
+		width: 100%;
+	    height: calc(100%);
+	    /*background: #007bff;*/
+	}
+	main#main{
+		width:100%;
+		height: calc(100%);
+		background:white;
+	}
+	#login-right{
+		position: absolute;
+		right:0;
+		width:40%;
+		height: calc(100%);
+		background:white;
+		display: flex;
+		align-items: center;
+	}
+	#login-left{
+		position: absolute;
+		left:0;
+		width:60%;
+		height: calc(100%);
+		background:#59b6ec61;
+		display: flex;
+		align-items: center;
+		background: url(../assets/img/1665046020_TRAVEL_CONCEPT.jpg);
+	    background-repeat: no-repeat;
+	    background-size: cover;
+	}
+	#login-right .card{
+		margin: auto;
+		z-index: 1
+	}
+	.logo {
+    margin: auto;
+    font-size: 8rem;
+    background: white;
+    padding: .5em 0.7em;
+    border-radius: 50% 50%;
+    color: #000000b3;
+    z-index: 10;
+}
+div#login-left::before,div#login-right::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: calc(100%);
+    height: calc(100%);
+    background: #00c4ff36;
+}
+
+</style>
 </html>

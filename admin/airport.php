5375cf8dbf58232fb2722b620707e8cb5e367a43
piyumi27 <piyurajakaruna@gmail.com>
2022-10-07 02:29:59 +0530

    Update airport.php

--- admin/airport.php
+++ admin/airport.php
@@ -156,6 +156,23 @@
         reader.readAsDataURL(input.files[0]);
     }
 }
+    function delete_airline($id){
+		start_load()
+		$.ajax({
+			url:'ajax.php?action=delete_airports',
+			method:'POST',
+			data:{id:$id},
+			success:function(resp){
+				if(resp==1){
+					alert_toast("Data successfully deleted",'success')
+					setTimeout(function(){
+						location.reload()
+					},1500)
+
+				}
+			}
+		})
+	}
 
 </script>
 							

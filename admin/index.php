240a9e879aab2c907c5c0f078e12375fd36eaf7e
yisirasankalapani4 <sankalpani.jayasuriya@gmail.com>
2022-10-06 23:04:39 +0530

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Admin | Online Flight Booking System</title>
+ 	
+
+<?php
+	session_start();
+  if(!isset($_SESSION['login_id']))
+    header('location:login.php');
+ include('./header.php'); 
+ // include('./auth.php'); 
+ ?>
+
+</head>

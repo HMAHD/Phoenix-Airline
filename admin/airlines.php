47fe18ea1a433659b112e00683044bc6cf43497f
piyumi27 <piyurajakaruna@gmail.com>
2022-10-07 04:08:58 +0530

    update airplanes.php

--- admin/airlines.php
+++ admin/airlines.php
@@ -27,7 +27,7 @@
 							
 							
              		</div>
-					
+
 					<div class="card-footer">
 						<div class="row">
 							<div class="col-md-12">
@@ -39,6 +39,48 @@
                 </div>
             </form>
             </div>
+			<!-- FORM Panel -->
+
+			<!-- Table Panel -->
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-body">
+						<table class="table table-bordered table-hover">
+							<thead>
+								<tr>
+									<th class="text-center">#</th>
+									<th class="text-center">Image</th>
+									<th class="text-center">Name</th>
+									<th class="text-center">Action</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php 
+								$i = 1;
+								$cats = $conn->query("SELECT * FROM airlines_list order by id asc");
+								while($row=$cats->fetch_assoc()):
+								?>
+								<tr>
+									<td class="text-center"><?php echo $i++ ?></td>
+									<td class="text-center">
+										<img src="../assets/img/<?php echo $row['logo_path'] ?>" alt="">
+									</td>
+									<td class="">
+										 <b><?php echo $row['airlines'] ?></b>
+									</td>
+									<td class="text-center">
+										<button class="btn btn-sm btn-primary edit_airline" type="button" data-id="<?php echo $row['id'] ?>" data-airlines="<?php echo $row['airlines'] ?>" data-logo_path="<?php echo $row['logo_path'] ?>" >Edit</button>
+										<button class="btn btn-sm btn-danger delete_airline" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
+									</td>
+								</tr>
+								<?php endwhile; ?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+			
+			<!-- Table Panel -->
 
         </div>
     </div>

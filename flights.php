8ae0f513aaef613b6a414864a731c5b6ea5e3a80
abhi-551 <abhi.adithya.wijewardhana@gmail.com>
2022-10-07 07:52:55 +0530

    Update flights.php

--- flights.php
+++ flights.php
@@ -124,4 +124,50 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 						<button class="btn-outline-primary  btn  mb-4 book_flight" type="button" data-id="<?php echo $row['id'] ?>"  data-name="<?php echo $aname[$row['departure_airport_id']].' - '.$aname[$row['arrival_airport_id']] ?>" data-max="<?php echo $row['seats'] - $booked ?>">Book Now</button>
 					</div>
 				</div>
+                <hr class="divider" style="max-width: 60vw">
+				<?php endwhile; ?>
+				<?php else: ?>
+					<div class="row align-items-center">
+						<h5 class="text-center"><b>No result.</b></h5>
+					</div>
+				<?php endif; ?>
+					
+				<?php if(isset($trip) && $trip ==2): ?>
+					<hr>
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<h2><b><?php echo isset($trip) && $trip == 2 ? "Arrival Searched Flight results..." : ( !isset($trip)? " Flights Available " :"Searched Flight results...")  ?></b></h2>
+						</div>
+					</div>
+						<hr class="divider">
+				<?php 
+				$airport = $conn->query("SELECT * FROM airport_list ");
+				while($row = $airport->fetch_assoc()){
+					$aname[$row['id']] = ucwords($row['airport'].', '.$row['location']);
+				}
+				$where = " where date(f.departure_datetime) > '".date("Y-m-d")."' ";
+				if($_SERVER['REQUEST_METHOD'] == "POST" )
+				$where .= " and f.departure_airport_id ='$arrival_airport_id' and f.arrival_airport_id = '$departure_airport_id' and date(f.departure_datetime) = '".date('Y-m-d',strtotime($date_return))."'  ";
+				$flight = $conn->query("SELECT f.*,a.airlines,a.logo_path FROM flight_list f inner join airlines_list a on f.airline_id = a.id $where order by rand()");
+				if($flight->num_rows > 0):
+				while($row=$flight->fetch_assoc()):
+					$booked = $conn->query("SELECT * FROM booked_flight where flight_id = ".$row['id'])->num_rows;
+
+				?>
+				<div class="row align-items-center">
+					<div class="col-md-3">
+						<img src="assets/img/<?php echo $row['logo_path'] ?>" alt="">
+					</div>
+					<div class="col-md-6">
+						 <p><b><?php echo $aname[$row['departure_airport_id']].' - '.$aname[$row['arrival_airport_id']] ?></b></p>
+						 <p><small>Airline: <b><?php echo $row['airlines'] ?></b></small></p>
+						 <p><small>Departure: <b><?php echo date('h:i A',strtotime($row['departure_datetime'])) ?></b></small></p>
+						 <p><small>Arrival: <b><?php echo (date('M d,Y',strtotime($row['departure_datetime'])) == date('M d,Y',strtotime($row['arrival_datetime']))) ? date('h:i A',strtotime($row['arrival_datetime'])) : date('M d,Y h:i A',strtotime($row['arrival_datetime'])) ?></b></small></p>
+						 <p>Available Seats : <b><h4><?php echo $row['seats'] - $booked ?></h4></b></p>
+					</div>
+					<div class="col-md-3 text-center align-self-end-sm">
+						<h4 class="text-right"><b><?php echo number_format($row['price'],2) ?></b></h4>
+						<button class="btn-outline-primary  btn  mb-4 book_flight" type="button" data-id="<?php echo $row['id'] ?>"  data-name="<?php echo $aname[$row['departure_airport_id']].' - '.$aname[$row['arrival_airport_id']] ?>" data-max="<?php echo $row['seats'] - $booked ?>">Book Now</button>
+					</div>
+				</div>
     </script>

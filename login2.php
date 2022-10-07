dfe582008a3e65b33c24ced880b6b2e704824bdf
nilupuldasun2000 <100019267+nilupuldasun2000@users.noreply.github.com>
2022-10-07 16:24:40 +0530

    Add

---
+++ login2.php
@@ -0,0 +1,30 @@
+<?php session_start() ?>
+<script>
+	$('#new_account').click(function(){
+		uni_modal("Create an Account",'signup.php?redirect=index.php?page=checkout')
+	})
+	$('#login-frm').submit(function(e){
+		e.preventDefault()
+		$('#login-frm button[type="submit"]').attr('disabled',true).html('Logging in...');
+		if($(this).find('.alert-danger').length > 0 )
+			$(this).find('.alert-danger').remove();
+		$.ajax({
+			url:'admin/ajax.php?action=login2',
+			method:'POST',
+			data:$(this).serialize(),
+			error:err=>{
+				console.log(err)
+		$('#login-frm button[type="submit"]').removeAttr('disabled').html('Login');
+
+			},
+			success:function(resp){
+				if(resp == 1){
+					location.href ='<?php echo isset($_GET['redirect']) ? $_GET['redirect'] : 'index.php?page=home' ?>';
+				}else{
+					$('#login-frm').prepend('<div class="alert alert-danger">Email or password is incorrect.</div>')
+					$('#login-frm button[type="submit"]').removeAttr('disabled').html('Login');
+				}
+			}
+		})
+	})
+</script>

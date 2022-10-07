f5c33ec29949a443f1e0c1156d7cc73069cd5a02
nilupuldasun2000 <100019267+nilupuldasun2000@users.noreply.github.com>
2022-10-07 16:24:08 +0530

    Add login

---
+++ login1.php
@@ -0,0 +1,21 @@
+<?php session_start() ?>
+<div class="container-fluid">
+	<form action="" id="login-frm">
+		<div class="form-group">
+			<label for="" class="control-label">Email</label>
+			<input type="email" name="email" required="" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="" class="control-label">Password</label>
+			<input type="password" name="password" required="" class="form-control">
+			<small><a href="javascript:void(0)" id="new_account">Create New Account</a></small>
+		</div>
+		<button class="button btn btn-info btn-sm">Login</button>
+	</form>
+</div>
+
+<style>
+	#uni_modal .modal-footer{
+		display:none;
+	}
+</style>

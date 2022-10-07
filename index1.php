8311c2b2ce39da9fbc2e52340c622069e7a6e26d
nilupuldasun2000 <100019267+nilupuldasun2000@users.noreply.github.com>
2022-10-07 15:35:22 +0530

    Add index

---
+++ index1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php
+    session_start();
+    ob_start();
+    include('header.php');
+    include('admin/db_connect.php');
+
+	$query = $conn->query("SELECT * FROM system_settings limit 1")->fetch_array();
+	foreach ($query as $key => $value) {
+		if(!is_numeric($key))
+			$_SESSION['setting_'.$key] = $value;
+	}
+    ob_end_flush();
+    ?>
+
+    <style>
+    	header.masthead {
+		  background: url(assets/img/<?php echo $_SESSION['setting_cover_img'] ?>);
+		  background-repeat: no-repeat;
+		  background-size: cover;
+		}
+    </style>
+</html>

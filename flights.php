29659af45b954ce0e90a6e453f1102f5652bea54
abhi-551 <abhi.adithya.wijewardhana@gmail.com>
2022-10-07 05:47:36 +0530

    Update flights.php

--- flights.php
+++ flights.php
@@ -7,4 +7,23 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 }
 
 ?>
+       <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4 page-title">
+                    	<h3 class="text-white">Flights</h3>
+                        <hr class="divider my-4" />
+                        <div class="col-md-12 mb-2 text-left">
+                        <div class="card">
+                            <div class="card-body">
+                                <form id="manage-filter"  action="index.php?page=flights" method="POST">
+                                    <div class="row form-group">
+                                        <div class="col-sm-3">
+                                            <label for="" class="control-label">From</label>
+                                            <select name="departure_airport_id" id="departure_location" class="custom-select browser-default select2">
+                                                <option value=""></option>
+                                            <?php
+                                                $airport = $conn->query("SELECT * FROM airport_list order by airport asc");
+                                            while($row = $airport->fetch_assoc()):
+                                            ?>
     </script>

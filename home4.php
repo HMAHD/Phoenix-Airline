71d3b61f20d66fda8c3563f5fcd27d1e81901e04
nilupuldasun2000 <100019267+nilupuldasun2000@users.noreply.github.com>
2022-10-07 11:25:10 +0530

    Add

---
+++ home4.php
@@ -0,0 +1,30 @@
+ <div class="row form-group">
+                                        <div class="col-sm-2 text-center">
+                                            <div class="form-check">
+                                              <input class="form-check-input" type="radio" name="trip" id="onewway" value="1" checked>
+                                              <label class="form-check-label" for="onewway">
+                                                One-way
+                                              </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-2 text-center">
+                                            <div class="form-check">
+                                              <input class="form-check-input" type="radio" name="trip" id="rtrip" value="2">
+                                              <label class="form-check-label" for="rtrip">
+                                               Round Trip
+                                              </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-3 offset-sm-5">
+                                            <button class="btn btn-block btn-sm btn-primary" type="submit"><i class="fa fa-plane-departure"></i> Find Flights</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>                        
+                    </div>
+                    
+                </div>
+            </div>
+        </header>

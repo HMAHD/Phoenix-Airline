c2d5a6ac8c3d68d178caec3bc243fc86c4849c1a
abhi-551 <abhi.adithya.wijewardhana@gmail.com>
2022-10-07 14:00:37 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,22 @@
+<style>
+   .modal-dialog.large {
+    width: 80% !important;
+    max-width: unset;
+  }
+  .modal-dialog.mid-large {
+    width: 50% !important;
+    max-width: unset;
+  }
+ </style>
+ <script>
+ 	$('.datepicker').datepicker({
+ 		format:"yyyy-mm-dd"
+ 	})
+ 	 window.start_load = function(){
+    $('body').prepend('<di id="preloader2"></di>')
+  }
+  window.end_load = function(){
+    $('#preloader2').fadeOut('fast', function() {
+        $(this).remove();
+      })
+  }

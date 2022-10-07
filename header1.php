e64b0c506f166ad0f45b45ce35a6e1a98860540a
Akash Hasendra <akashhasendra2@gmail.com>
2022-10-07 11:02:30 +0530

    Relocate

--- header1.php
+++
@@ -1,7 +0,0 @@
-<meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="Akash hasendra" />
-        <title><?php echo $_SESSION['setting_name'] ?></title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />

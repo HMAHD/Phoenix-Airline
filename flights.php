b1405dcd50fd55fec8774b6d024824dddea64ac5
abhi-551 <abhi.adithya.wijewardhana@gmail.com>
2022-10-07 07:26:10 +0530

    Update flights.php

--- flights.php
+++ flights.php
@@ -53,4 +53,35 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
                                             <input type="date" class="form-control input-sm datetimepicker2" name="date_return" autocomplete="off" value="<?php echo isset($date_return) && !empty($date_return) ? date("Y-m-d",strtotime($date_return)) : "" ?>">
                                         </div>
                                     </div>
+                                    <div class="row form-group">
+                                        <div class="col-sm-2 text-center">
+                                            <div class="form-check">
+                                              <input class="form-check-input" type="radio" name="trip" id="onewway" value="1" <?php echo isset($trip) && $trip == 1 ? "checked" : "" ?> >
+                                              <label class="form-check-label" for="onewway">
+                                                One-way
+                                              </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-2 text-center">
+                                            <div class="form-check">
+                                              <input class="form-check-input" type="radio" name="trip" id="rtrip" value="2" <?php echo isset($trip) && $trip == 2 ? "checked" : "" ?>>
+                                              <label class="form-check-label" for="rtrip">
+                                               Round Trip
+                                              </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-3 offset-sm-5">
+                                            <button class="btn btn-block btn-sm btn-primary"><i class="fa fa-plane-departure"></i> Find Flights</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>                        
+                    </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
     </script>

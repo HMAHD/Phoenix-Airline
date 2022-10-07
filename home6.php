50d985013749e375bc782e5d55af48ea616f5b51
nilupuldasun2000 <100019267+nilupuldasun2000@users.noreply.github.com>
2022-10-07 11:26:19 +0530

    Add

---
+++ home6.php
@@ -0,0 +1,24 @@
+<?php endwhile; ?>
+                    
+                </div>
+            </div>
+        </div>
+    <script>
+        
+        $('.view_prod').click(function(){
+            uni_modal_right('Product','view_prod.php?id='+$(this).attr('data-id'))
+        })
+        $('.select2').select2({
+            placeholder:'Select Departure',
+            width:'100%'
+        })
+        $('[name="trip"]').on("keypress change keyup",function(){
+            if($(this).val() == 1){
+                $('#rdate').hide()
+            }else{
+                $('#rdate').show()
+            }
+        })
+    </script>
+	
+    </section>

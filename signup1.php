fd08550bbf684a951a16a5b9d06e3c1cb4ea3a3f
nilupuldasun2000 <100019267+nilupuldasun2000@users.noreply.github.com>
2022-10-07 16:25:16 +0530

    Add signup

---
+++ signup1.php
@@ -0,0 +1,25 @@
+<div class="container-fluid">
+	<form action="" id="signup-frm">
+		<div class="form-group">
+			<label for="" class="control-label">Name</label>
+			<input type="text" name="name" required="" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="" class="control-label">Contact</label>
+			<input type="text" name="contact" required="" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="" class="control-label">Address</label>
+			<textarea cols="30" rows="3" name="address" required="" class="form-control"></textarea>
+		</div>
+		<div class="form-group">
+			<label for="" class="control-label">Email</label>
+			<input type="email" name="email" required="" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="" class="control-label">Password</label>
+			<input type="password" name="password" required="" class="form-control">
+		</div>
+		<button class="button btn btn-info btn-sm">Create</button>
+	</form>
+</div>
